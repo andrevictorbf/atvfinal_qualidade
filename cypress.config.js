057792911b0da541e2e76d6661085ef2641d5b53
IgorBrito02 <igor_pbrito@hotmail.com>
2025-11-30 23:48:05 -0300

    feat: Configuração inicial do Cypress (Base URL e Reporters)

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,20 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    // Base URL do projeto (usar cy.visit('/'))
+    baseUrl: 'https://automationexercise.com/',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Aqui podem ser registrados event listeners do Node (se necessário)
     },
+    // Localização dos specs (e2e + api)
+    specPattern: 'cypress/{e2e,api}/**/*.cy.{js,jsx,ts,tsx}'
   },
+  // Reporter para geração de relatório em CI
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/results',
+    overwrite: false,
+    html: false,
+    json: true
+  }
 });
